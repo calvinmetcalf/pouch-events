1c83840f01498ebfa425f7651b7d6fb60cfcf8ce
Calvin Metcalf <cmetcalf@appgeo.com>
2013-12-08 22:15:54 -0500

    all but destroy work

--- index.js
+++ index.js
@@ -7,37 +7,39 @@ function PouchEvents(db) {
   if(!(this instanceof PouchEvents)){
     return new PouchEvents(db);
   }
-  var self = this;
   db.info(function(err, info) {
     if(err){
-      self.emit('error',err);
+      db.emit('error',err);
       return;
     }
-    DBs[info.db_name] = self;
+    DBs[info.db_name] = db;
     db.changes({
         conflicts: true,
         include_docs: true,
         continuous:true,
         since:info.update_seq,
         onChange: function (change) {
-          self.emit('change',change);
-          if('deleted' in change){
-            self.emit('delete',change.doc);
-          } else if(change.doc._rev.split('_')[0]==='1'){
-            self.emit('create',change.doc);
+          db.emit('change',change);
+          if(change.doc._deleted){
+            db.emit('delete',change.doc);
+          } else if(change.doc._rev.split('-')[0]==='1'){
+            db.emit('create',change.doc);
           } else {
-            self.emit('update',change.doc);
+            db.emit('update',change.doc);
           }
-      });
+        }
+    });
   });
-};
-PouchEvents._destroy = function(name){
+
+}
+util.inherits(PouchEvents, events.EventEmitter);
+PouchEvents._delete = function(name){
   if(name in DBs){
-    DBs[name].emit('destroy');
+    DBs[name].emit('destroy',true);
     DBs[name].removeAllListeners();
     delete DBs[name];
   }
 }
-util.inherits(PouchEvents, events.EventEmitter);
+
 
 module.exports = PouchEvents;
--- test/test.js
+++ test/test.js
@@ -3,3 +3,82 @@ var Events = require('../');
 Pouch.plugin('events',Events);
 var should = require('chai').should();
 
+describe('events',function(){
+  var root = '';
+  var i = 0;
+  var db;
+  beforeEach(function(done){
+    i++;
+    Pouch(root + 'dbtest' + i,function(err,d){
+      if(err){
+        console.log(err);
+        return done(err);
+      }
+      db = d;
+      done();
+    });
+  });
+  afterEach(function(done){
+    db = undefined;
+    Pouch.destroy(root + 'dbtest' + i,function(){
+      done();
+    });
+  });
+  it('change works',function(done){
+    db.on('change',function(){
+      done();
+    });
+    db.put({
+      '_id':'hello'
+    });
+  });
+  it('create works',function(done){
+    db.on('create',function(){
+      done();
+    });
+    db.put({
+      '_id':'hello'
+    });
+  });
+  it('update works',function(done){
+    db.on('update',function(){
+      done();
+    });
+    db.put({
+      '_id':'hello'
+    },function(err,resp){
+      if(err){
+        done(err);
+      }
+      db.get('hello',function(err,doc){
+        if(err){
+          done(err);
+        }
+        doc.blah='abc';
+        db.put(doc);
+      })
+    });
+  });
+  it('delete works',function(done){
+    db.on('delete',function(){
+      done();
+    });
+    db.put({
+      '_id':'hello'
+    },function(err,resp){
+      if(err){
+        done(err);
+      }
+      db.get('hello',function(err,doc){
+        if(err){
+          done(err);
+        }
+        db.remove(doc);
+      })
+    });
+  });
+  // it("should work",function(done){
+  //     db.on('destroy',function(){done();});
+  //     Pouch.destroy('dbtest5');
+  //   });
+})
